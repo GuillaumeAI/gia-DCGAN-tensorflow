2ffd032cb2e13ff606b672274433b7588e396119
Jean Guillaume <jgi@jgwill.com>
2021-01-08 20:30:44 -0500

    update

--- gaia-run-docker.sh
+++ gaia-run-docker.sh
@@ -1,5 +1,5 @@
 
-export lib_path=/mnt/c/Users/jeang/Dropbox/lib
+export lib_path=/mnt/c/lib
 export model_path=/mnt/c/model
 export container_tag=guillaumai/tf:0.12-cpu
 docker run -it --rm -v $(pwd):/work -v $lib_path:/a/lib -v $model_path:/a/model  $container_tag
--- sample-train.sh
+++ sample-train.sh
@@ -1,1 +1,2 @@
 python main.py --dataset giapicassov3 --input_height=700 --train --crop
+python main.py --dataset giapicassov3 --input_height=700 --train --crop --out_dir /a/model/models/x__gan__v01__2101082004 --out_name giapicassov3 --input_width=700 --input_fname_pattern *.jpg
